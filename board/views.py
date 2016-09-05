b0c28d2a6b6e35c7f18430b0a14684805a960e69
Ramiro Alvaro <ramiroalvaro@hotmail.com>
2016-09-05 13:54:06 -0300

    Serializando User

--- board/views.py
+++ board/views.py
@@ -1,10 +1,27 @@
-from rest_framework import viewsets
+from rest_framework import authentication, permissions, viewsets
 
 from .models import Sprint
 from .serializers import SprintSerializer
 
 
-class SprintViewSet(viewsets.ModelViewSet):
+class DefaultsMixin(object):
+    """Configuraciones default para autenticacion, permisos, filtros y paginacion de la view"""
+
+    authentication_classes = (
+        authentication.BasicAuthentication,
+        authentication.TokenAuthentication,
+    )
+
+    permissions_classes = (
+        permissions.IsAuthenticated,
+    )
+
+    paginate_by = 25
+    paginate_by_param = 'page_size'
+    max_paginate_by = 100
+
+
+class SprintViewSet(DefaultsMixin, viewsets.ModelViewSet):
     """Endpoint de la API para listar y crear sprints."""
 
     queryset = Sprint.objects.order_by('end')
